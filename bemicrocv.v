d32ffae8cc46214c4e967d13ad0ee511705d6451
Tommy Thorn <tommy-github2@thorn.ws>
2014-02-25 11:12:12 -0800

    Give the rationale for the loop

--- bemicrocv.v
+++ bemicrocv.v
@@ -1,22 +1,26 @@
 module bemicrocv
 
-  (input        clk_50,
-   input  [2:0] user_dipsw_n,
-   input  [1:0] user_button_n,
-   output [7:0] user_led_n);
+  (input         clk_50,
+   input  [ 2:0] user_dipsw_n,
+   input  [ 1:0] user_button_n,
+   output [ 7:0] user_led_n);
 
-   reg [7:0]    cnt;
-   reg [31:0]   divide;
+   reg    [ 7:0] cnt;
+   reg    [31:0] divide;
 
-   assign       user_led_n = ~cnt;
-   // {user_button_n, user_dipsw_n, ~cnt};
+   assign        user_led_n = ~cnt;
 
    always @(posedge clk_50)
+     // Note, this style of loops are very efficient as uses a single
+     // bit (the sign bit) for control as opposed to, say, divide == 50_000_000
+     // which is quite expensive.  However, as we only roll around when we hit -1
+     // we have start at N-2 to get exactly N iterations.
+
      if (divide[31]) begin
-	cnt <= cnt + 1 'd 1;
-	divide <= 50_000_00-2;
+	cnt <= cnt + 1'd1;
+	divide <= 50_000_00 - 2;
      end
      else
-       divide <= divide - 1;
+        divide <= divide - 1;
 
 endmodule
